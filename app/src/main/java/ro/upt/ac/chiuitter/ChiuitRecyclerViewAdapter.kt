0dc98c91c8a876699bb42017b4cb91a1c78d6bfb
Cristian <cristian10.sicoe@gmail.com>
2021-11-13 16:09:55 +0200

    List activity commit

--- app/src/main/java/ro/upt/ac/chiuitter/ChiuitRecyclerViewAdapter.kt
+++ app/src/main/java/ro/upt/ac/chiuitter/ChiuitRecyclerViewAdapter.kt
@@ -1,5 +1,6 @@
 package ro.upt.ac.chiuitter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -11,19 +12,26 @@ class ChiuitRecyclerViewAdapter(
     : RecyclerView.Adapter<ChiuitRecyclerViewAdapter.ChiuitViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChiuitViewHolder {
-        TODO("8. Inflate the item layout and return the view holder")
+        //TODO("8. Inflate the item layout and return the view holder")
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_chiuit, parent, false)
+        return ChiuitViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("9. Return the size of samples")
+       // TODO("9. Return the size of samples")
+        return chiuitList.size
     }
 
     override fun onBindViewHolder(holder: ChiuitViewHolder, position: Int) {
-        TODO("10. Bind the view holder with chiuit data sample")
+       // TODO("10. Bind the view holder with chiuit data sample")
+        holder.bind(chiuitList[position])
     }
 
     fun addItem(chiuit: Chiuit) {
-        TODO("12. Add the new item to the list then SMARTLY notify an addition")
+       // TODO("12. Add the new item to the list then SMARTLY notify an addition")
+        chiuitList.add(chiuit)
+        notifyItemInserted(chiuitList.size - 1)
     }
 
     inner class ChiuitViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -33,7 +41,8 @@ class ChiuitRecyclerViewAdapter(
         }
 
         fun bind(chiuit: Chiuit) {
-            TODO("11. Set the text view with the content of chiuit's description")
+            //TODO("11. Set the text view with the content of chiuit's description")
+            itemView.txv_content.text = chiuit.description
         }
 
     }
--- app/src/main/java/ro/upt/ac/chiuitter/HomeActivity.kt
+++ app/src/main/java/ro/upt/ac/chiuitter/HomeActivity.kt
@@ -4,6 +4,10 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.item_chiuit.*
+import kotlinx.android.synthetic.main.view_home.*
+import ro.upt.ac.chiuitter.ComposeActivity.Companion.EXTRA_TEXT
 
 class HomeActivity : AppCompatActivity() {
 
@@ -14,14 +18,21 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.view_home)
-
+        fab_add.setOnClickListener {
+            composeChiuit()
+        }
         initList()
     }
 
     private fun initList() {
         val chiuitList = dummyChiuitStore.getAllData()
 
-        TODO("7. Instantiate the adapter, then setup the recycler view list")
+        //TODO("7. Instantiate the adapter, then setup the recycler view list")
+        listAdapter = ChiuitRecyclerViewAdapter(chiuitList.toMutableList()) {
+            shareChiuit(it.description)
+        }
+        rv_chiuit_list.adapter = listAdapter
+        rv_chiuit_list.layoutManager = LinearLayoutManager(this)
     }
 
     /*
@@ -31,8 +42,9 @@ class HomeActivity : AppCompatActivity() {
     private fun shareChiuit(text: String) {
         val sendIntent = Intent().apply {
             // TODO 1: Configure to support text sending/sharing and then attach the text as intent's extra.
-
-
+            action = Intent.ACTION_SEND
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, text)
         }
 
         val intentChooser = Intent.createChooser(sendIntent, "")
@@ -45,10 +57,11 @@ class HomeActivity : AppCompatActivity() {
      */
     private fun composeChiuit() {
         // TODO 2: Create an explicit intent which points to ComposeActivity.
-
+        val explicitIntent = Intent(this, ComposeActivity::class.java)
 
         // TODO 3: Start a new activity with the previously defined intent.
         // We start a new activity that we expect to return the acquired text as the result.
+        startActivityForResult(explicitIntent, COMPOSE_REQUEST_CODE)
 
     }
 
@@ -62,12 +75,14 @@ class HomeActivity : AppCompatActivity() {
     private fun extractText(data: Intent?) {
         data?.let {
             // TODO 5: Extract the text from result intent.
-
+            val textFromResultIntent : String? = data.getStringExtra(EXTRA_TEXT)
 
             // TODO 6: Check if text is not null or empty, then set the new "chiuit" content.
 
+            if (!textFromResultIntent.isNullOrEmpty())
+                listAdapter.addItem(Chiuit(textFromResultIntent))
 
-            TODO("13. Instantiate a new chiuit object that add it to the adapter")
+            //TODO("13. Instantiate a new chiuit object that add it to the adapter")
         }
     }
 
